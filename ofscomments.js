06c9687648b69510b82316badbc435a0a53fcbe3
Lincoln Atkinson <latkin@users.noreply.github.com>
2017-09-29 07:38:50 -0700

    Client can submit comments directly from the page

--- ofscomments.js
+++ ofscomments.js
@@ -5,8 +5,10 @@ var ofs_postId = null;
 function OFSCommentsInit(postId) {
     ofs_postId = postId;
     $("#ofs-comments").append("<div id='ofs-commentlist'/>");
+    $("#ofs-comments").append("<div id='ofs-submitcomment'/>");
 
     LoadComments();
+    LoadCommentSubmission();
 }
 
 // pulls down array of comments for this post and inserts them
@@ -25,6 +27,36 @@ function LoadComments() {
     });
 }
 
+// adds the comment submission text boxes
+// and buttons into the page
+function LoadCommentSubmission() {
+    var t = "<form id='ofs-submit-form'>";
+    t += "<fieldset>";
+    t += "<legend>Leave a comment</legend>";
+    t += "Name<br>";
+    t += "<input type='text' id='ofs-submit-name'><br>";
+    t += "Comment<br>";
+    t += "<textarea id='ofs-submit-comment' cols='80' rows='10' /><br><br>";
+    t += "<input type='submit' id='ofs-submit-postcomment' value='Post comment'>";
+    t += "</fieldset>";
+    t += "</form>";
+
+    $("#ofs-submitcomment").append(t);
+
+    $("#ofs-submit-postcomment").click(function (e) {
+        e.preventDefault();
+        SubmitAndRenderComment(ofs_postId, $("#ofs-submit-name").val(), $("#ofs-submit-comment").val());
+    });
+}
+
+// helper to disable/enable the various text boxes and buttons
+// used for submitting comments
+function ToggleSubmissionControls(enabled) {
+    $("#ofs-submit-name").prop("disabled", !enabled);
+    $("#ofs-submit-comment").prop("disabled", !enabled);
+    $("#ofs-submit-postcomment").prop("disabled", !enabled);
+}
+
 // inserts a single comment
 function AddSingleComment(comment) {
     var date = new Date(comment.time);
@@ -36,4 +68,31 @@ function AddSingleComment(comment) {
     t += comment.comment;
     t += "</div>";
     $("#ofs-commentlist").append(t);
+}
+
+// submits a single comment to the server
+function SubmitAndRenderComment(postId, name, comment) {
+    ToggleSubmissionControls(false);
+
+    $.ajax(azureFuncUrl, {
+        data: JSON.stringify({ postid: postId, name: name, comment: comment }),
+        type: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data, status, xhr) {
+            AddSingleComment(data);
+            $("#ofs-submit-name").val("");
+            $("#ofs-submit-comment").val("");
+            ToggleSubmissionControls(true);
+        },
+        error: function (err) {
+            var data = $.parseJSON(err.responseText);
+            if (data.Message) {
+                $("#ofs-commentlist").append("<div class='ofs-error'>Error adding comment: " + data.Message + "</div>");
+            } else {
+                $("#ofs-commentlist").append("<div class='ofs-error'>Error adding comment</div>");
+            }
+            ToggleSubmissionControls(true);
+        }
+    });
 }
